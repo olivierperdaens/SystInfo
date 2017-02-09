33da2fc07828d973e1f1b325bc0a071d6d2ddda5
donaschmi <donatien@schmitz.cc>
2017-02-09 08:36:08 +0000

    correction test.c

--- TP1/test.c
+++ TP1/test.c
@@ -2,12 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, const char *argv[]){
-  if(argc<4)
-    return EXIT_FAILURE;
-  int sc=strcmp(argv[1], argv[2]);
-  if(sc==1) printf("Equal");
-  else
-    printf("Not Equal");
-  return EXIT_SUCCESS;
+int eq(int a, int b){
+    if(a==b) return 0;
+    return 1;
 }
+
+int ge(int a, int b){
+    if(a>=b) return 0;
+    return 1;
+}
+
+int gt(int a, int b){
+    if(a>b) return 0;
+    return 1;
+}
+
+int le(int a, int b){
+    if(a<=b) return 0;
+    return 1;
+}
+
+int lt(int a, int b){
+    if(a<b) return 0;
+    return 1;
+}
+
+int ne(int a, int b){
+    if(a==b) return 1;
+    return 0;
+}
+
+int main(int argc, const char *argv[]){
+    if(strcmp(argv[2], "-eq")==0){
+        return eq(atoi(argv[1]), atoi(argv[3]));
+    }
+    if(strcmp(argv[2], "-ge")==0){
+        return ge(atoi(argv[1]), atoi(argv[3]));
+    }
+    if(strcmp(argv[2], "-gt")==0){
+        return gt(atoi(argv[1]), atoi(argv[3]));
+    }
+    if(strcmp(argv[2], "-le")==0){
+        return le(atoi(argv[1]), atoi(argv[3]));
+    }
+    if(strcmp(argv[2], "-lt")==0){
+        return lt(atoi(argv[1]), atoi(argv[3]));
+    }
+    if(strcmp(argv[2], "-ne")==0){
+        return ne(atoi(argv[1]), atoi(argv[3]));
+    }
+    return EXIT_FAILURE;
+}
+
