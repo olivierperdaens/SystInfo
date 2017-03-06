2dc8b0f72613686dd23c1b3ed6f828b700a62ddd
donaschmi <donatien@schmitz.cc>
2017-03-06 13:53:36 +0000

    ajout test

--- Projet1/matrix.c
+++ Projet1/matrix.c
@@ -387,7 +387,7 @@ void print(const struct matrix *matrix){
 int main(){
 
     int *array[3];
-
+    int *array1[3];
     int arr_1[4] = { 1, 2, 3, 4, };
     int arr_2[4] = { 5, 6, 7, 8, };
     int arr_3[4] = { 9, 10, 11, 12, };
@@ -395,11 +395,26 @@ int main(){
     array[0] = arr_1;
     array[1] = arr_2;
     array[2] = arr_3;
-    
+
+    array1[0]=arr_2;
+    array1[1]=arr_3;
+    array1[2]=arr_1;
+
     struct matrix* mat=matrix_convert((const int**)array,3,4);
     print(mat);
+    printf("\n");
+    struct matrix* mat1=matrix_convert((const int**)array1,3,4);
+    print(mat1);
+    printf("\n");
+    struct matrix* mat2=matrix_add( mat,mat1);
+    print(mat2);
+    printf("\n");
+    struct matrix* mat3=matrix_transpose(mat);
+    print(mat3);
     free(mat);
-
+    free(mat1);
+    free(mat2);
+    free(mat3);
 
     /*struct matrix * mat = matrix_init(5,4);
     int a=matrix_set(mat, 1,2,1);
