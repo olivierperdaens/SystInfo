fe9bc56312939cf9a85657ea54a9c76f8b4c2c09
donaschmi <donatien@schmitz.cc>
2017-03-06 12:32:12 +0000

    reste à supprimer les lines si val==0

--- Projet1/matrix.c
+++ Projet1/matrix.c
@@ -114,6 +114,11 @@ int line_checkelem(const struct line* line, unsigned int j){
     return 0;
 }
 
+/* matrix_zeroline
+ * Après attribution de la valeur 0 à la ligne d'index i, 
+ * vérifie si la ligne ne contient que des 0
+ *
+ */
 
 /* matrix_set
  * Défini la valeur d'un élément de la matrice.
@@ -154,12 +159,15 @@ int matrix_set(struct matrix *matrix, unsigned int i, unsigned int j, int val){
                     else{
                         cline->elems=celem->next;
                         free(celem);
+                        //matrix_zeroline(matrix, i);
                         return 0;
                     }
                 }
                 while (celem->j!=j){
                     celem=celem->next;
                 }
+                celem->value=val;
+                return 0;
             }
             //l'element n'existe pas, on va l'insérer
             else{
@@ -194,7 +202,7 @@ int matrix_set(struct matrix *matrix, unsigned int i, unsigned int j, int val){
                         celem->next=elem;
 
                     }
-               }
+                }
             }
         }
         //la ligne n'existe pas
@@ -202,6 +210,33 @@ int matrix_set(struct matrix *matrix, unsigned int i, unsigned int j, int val){
             //Si val vaut 0, alors comme la ligne n'existe pas, il ne faut pas la créer
             if(val==0) return 0;
             else{
+                struct elem* elem=(struct elem*) malloc(sizeof(struct elem));
+                elem->j=j;
+                elem->value=val;
+                elem->next=NULL;
+
+                struct line* line=(struct line*) malloc(sizeof(struct line));
+                line->i=i;
+                line->elems=elem;
+                if(matrix->lines->i>i){
+
+                    line->next=matrix->lines;
+                    matrix->lines=line;
+                    return 0;
+                }
+                else{
+                    struct line* cline=matrix->lines;
+                    struct line* nline=cline->next;
+                    while(nline!=NULL){
+                        if(nline->i>i){
+                            cline=nline;
+                            nline=nline->next;
+                        }
+                        else break;
+                    }
+                    line->next=nline;
+                    cline->next=line;
+                }
 
             }
 
@@ -224,8 +259,7 @@ int matrix_set(struct matrix *matrix, unsigned int i, unsigned int j, int val){
     //TODO une fois l'élément inséré, si val vaut 0, on vérifie que la ligne n'est pas nulle
     //     Si c'est le cas, on la supprime
     //
-  
-   
+
     return 0;   
 }
 
@@ -351,19 +385,39 @@ void print(const struct matrix *matrix){
 }
 
 int main(){
-    struct matrix * mat = matrix_init(5,4);
+
+    int *array[3];
+
+    int arr_1[4] = { 1, 2, 3, 4, };
+    int arr_2[4] = { 5, 6, 7, 8, };
+    int arr_3[4] = { 9, 10, 11, 12, };
+
+    array[0] = arr_1;
+    array[1] = arr_2;
+    array[2] = arr_3;
+    
+    struct matrix* mat=matrix_convert((const int**)array,3,4);
+    print(mat);
+    free(mat);
+
+
+    /*struct matrix * mat = matrix_init(5,4);
     int a=matrix_set(mat, 1,2,1);
     printf("a = %d\n",a);
     print(mat);
     a=matrix_set(mat ,1,1,3);
-    //printf("a = %d\n",a);
+    printf("\n");
     print(mat);
-        //struct line* line1=mat->lines;
+    a=matrix_set(mat,2,1,1);
+    printf("\n");
+
+    print(mat);
+    //struct line* line1=mat->lines;
     //struct line* line2=line1->next;
     //printf("index of first line : %d, %d \n",line1->i,line1->elems->j);
     //printf("index of second line : %d, %d \n",line2->i,line2->elems->j);
 
-    matrix_free(mat);
+    matrix_free(mat);*/
     //free(elem1);
     //free(line1);
     //free(mat);
