5617b247d8e708c6810a3e2ed9e408bb14ad2c6b
donaschmi <donatien@schmitz.cc>
2017-06-02 13:37:55 +0000

    ajout test Fichiers

---
+++ Inginious/Fichiers/filemap.c
@@ -0,0 +1,28 @@
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+int count(char *filename, char c) {
+    int count_char=0;
+    int cha;
+    FILE* file=fopen(filename,"r");
+    if(file){
+        while((cha=getc(file))!=EOF){
+            if(c==cha) count_char++;
+        }
+        fclose(file);
+        return count_char;
+    }
+    return 0;
+}
+
+int main(){
+    char a='a';
+    int count_c=count("test.txt",a);
+    printf("Il y a %d fois le char %c dans test.txt \n",count_c,a);
+    return EXIT_SUCCESS;
+}
